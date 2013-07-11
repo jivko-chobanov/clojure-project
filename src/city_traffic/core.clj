982ae9071dc8e40837b785e47cc74020566f01a3
Zhivko Chobanov <zhizho91@gmail.com>
2013-07-11 12:44:58 +0300

    roads are not parallel

--- src/city_traffic/core.clj
+++ src/city_traffic/core.clj
@@ -6,6 +6,8 @@
 (def car-length 1)
 (def min-road-length (* 2 car-length))
 (def max-road-length (* 5 car-length))
+(def min-road-tilt (* 3 car-length))
+(def max-road-tilt-deviation (* 9 car-length))
 
 (def animation-sleep-ms 100)
 (def car-sleep-ms 40)
@@ -21,10 +23,10 @@
 
 (defn delta-loc [[x y] dir]
   (case dir
-    0 [x (inc y)]
-    2 [(inc x) y]
-    4 [x (dec y)]
-    6 [(dec x) y]))
+    0 [[x (inc y)] [(dec x) (inc y)] [(inc x) (inc y)]]
+    2 [[(inc x) y] [(inc x) (dec y)] [(inc x) (inc y)]]
+    4 [[x (dec y)] [(dec x) (dec y)] [(inc x) (dec y)]]
+    6 [[(dec x) y] [(dec x) (dec y)] [(dec x) (inc y)]]))
 
 (defn move [old-place new-place]
   (let [car (:car @old-place)]
@@ -32,22 +34,33 @@
     (alter new-place assoc :car car)))
 
 (defn behave [car coordinates]
-  (let [p (place coordinates)
-        ahead-coordinates (delta-loc coordinates (:dir car))]
-    (when (every? #(and (>= % 0) (< % dim)) ahead-coordinates)
-      (let [ahead (place ahead-coordinates)
-            ahead-map @ahead]
-        (Thread/sleep car-sleep-ms)
-        (when running
-          (if (:crossing ahead-map)
+  (when running
+    (let [p (place coordinates)
+          ahead-coordinate-pairs-in-board (filter
+                                            (fn [ahead-coordinates]
+                                              (every? #(and (>= % 0) (< % dim)) ahead-coordinates))
+                                            (delta-loc coordinates (:dir car)))
+          ahead-coordinate-pair-with-crossing (first (filter
+                                                       (fn [ahead-coordinates]
+                                                         (let [ahead-info @(place ahead-coordinates)]
+                                                           (or (:crossing ahead-info) (:tr-light ahead-info))))
+                                                       ahead-coordinate-pairs-in-board))
+          ahead-coordinate-pair-with-road (first (filter #(:road @(place %)) ahead-coordinate-pairs-in-board))]
+      (Thread/sleep car-sleep-ms)
+      (if ahead-coordinate-pair-with-crossing
+        (let [ahead (place ahead-coordinate-pair-with-crossing)
+              ahead-info @ahead]
+          (if (:crossing ahead-info)
             nil
-            (if (:tr-light ahead-map)
-              nil
-              (when (:road ahead-map)
-                (dosync
-                  (move p ahead)
-                  (send-off *agent* behave [(:x ahead-map) (:y ahead-map)]))
-                car))))))))
+            (if (:tr-light ahead-info)
+              nil)))
+        (if ahead-coordinate-pair-with-road
+          (let [ahead (place ahead-coordinate-pair-with-road)
+                ahead-info @ahead]
+            (dosync
+              (move p ahead)
+              (send-off *agent* behave [(:x ahead-info) (:y ahead-info)]))
+            car))))))
 
 (defn build-road-on-place [x y]
   (let [p (place [x y])]
@@ -61,14 +74,25 @@
   (dosync
     (doseq [x-fixed? [true false]]
       (loop [last-fixed-dim 1] ;roads cannot be borders of space, beacause of car starting
-        (let [fixed-dim (+ min-road-length last-fixed-dim (rand-int (+ 1 max-road-length)))]
-          (doseq [free-dim (range dim)]
-            (if x-fixed?
-              (build-road-on-place fixed-dim free-dim)
-              (build-road-on-place free-dim fixed-dim)))
-          (when (< (+ min-road-length fixed-dim max-road-length) dim--)
-            ;roads cannot be borders of space, beacause of car startin
-            (recur fixed-dim)))))))
+        (if (zero? (rand-int 2))
+          (let [tilt-deviation (rand-int (min max-road-tilt-deviation (- dim last-fixed-dim 1)))
+                fixed-dim (+ min-road-length last-fixed-dim (rand-int (+ 1 max-road-length)))]
+            (doseq [free-dim (range dim)]
+              (let [tilted-fixed-dim (+ fixed-dim (quot (* (+ tilt-deviation min-road-tilt) free-dim) dim))]
+                (if x-fixed?
+                  (build-road-on-place tilted-fixed-dim free-dim)
+                  (build-road-on-place free-dim tilted-fixed-dim))))
+            (when (< (+ min-road-length fixed-dim tilt-deviation min-road-tilt max-road-length) dim--)
+              ;roads cannot be borders of space, beacause of car startin
+              (recur (+ fixed-dim tilt-deviation min-road-tilt))))
+          (let [fixed-dim (+ min-road-length last-fixed-dim (rand-int (inc max-road-length)))]
+            (doseq [free-dim (range dim)]
+              (if x-fixed?
+                (build-road-on-place fixed-dim free-dim)
+                (build-road-on-place free-dim fixed-dim)))
+            (when (< (+ min-road-length fixed-dim max-road-length) dim--)
+              ;roads cannot be borders of space, beacause of car startin
+              (recur fixed-dim))))))))
 
 (defn setup-car [[x y :as coordinates] direction]
   (let [car (agent {:img 0
